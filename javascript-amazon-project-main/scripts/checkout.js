918fa4ef65d329fa9c31d50032b17b9087c167d8
Eray TURAN <turanerayy@gmail.com>
2025-03-08 17:15:13 +0300

    convert from promises to async await

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -5,6 +5,43 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+/*
+function loadPage(){
+    return new Promise((resolve) => {
+        console.log('load page');
+        resolve();
+    }).then(() => {
+        return loadProductsFetch();
+    }).then(() => {
+        return new Promise((resolve) => {
+            resolve('value2');
+        });
+    });
+}
+*/
+
+// async fonksiyonlar return olarak new Promise dondururler. Tekrar tekrar new promise yazmaya gerek kalmaz.
+// await async ile birlikte kullanilir. promise in bitmesini bekler yeni satira gecmek icin.
+// async await can only be used with Promises.
+// bu fonksiyon yukarıdaki fonksiyonun kisayoludur.
+async function loadPage(){
+
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+});
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+
+loadPage();
+
+/*
 Promise.all([
     loadProductsFetch(),
      new Promise((resolve) => {
@@ -18,6 +55,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 })
+*/
 
 // Promise asenkron kodlarda bitimin beklenmesi için callback lere gore daha etkili bir yontemdir. Callbacklerde aynı gorevi yapar ama nesting
 // yada projenin ileri zamanlarında zorluklar cıkarabilir. 
